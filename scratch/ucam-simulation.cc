2b957cfa8e07514822e23e71c0ed578c745cddbe
Carlos Rocha <carlosh2340@gmail.com>
2020-11-02 00:25:20 -0300

    Initial commit
    - Basic scenario implemented
    - Evalvid submodule added
    - Relays start in off state

---
+++ scratch/ucam-simulation.cc
@@ -0,0 +1,229 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ucam-simulation.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/10/2020 22:23:17
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Carlos Rocha, carlosh2340@gmail.com
+ *        Company:  Gercom
+ *
+ * =====================================================================================
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "ns3/mesh-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/mesh-helper.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/wifi-net-device.h"
+#include "ns3/wifi-phy.h"
+#include "ns3/evalvid-client-server-helper.h"
+#include "ns3/netanim-module.h"
+
+using namespace ns3;
+
+unsigned int simTime = 100;
+uint16_t nTrackers = 2;
+short nRelays = 10;
+short nVictims = 2;
+bool enableNetAnim = false;
+
+void installMobility( NodeContainer firstResponders, NodeContainer drones, NodeContainer victims)
+{
+	MobilityHelper mobility;
+	mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+	mobility.Install (firstResponders);
+
+	mobility.SetMobilityModel("ns3::WaypointMobilityModel",
+								"InitialPositionIsWaypoint", BooleanValue (true));
+	mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+								   "MinX", DoubleValue (-30.0),
+								   "MinY", DoubleValue (-30.0),
+								   "DeltaX", DoubleValue (20.0),
+								   "DeltaY", DoubleValue (20.0),
+								   "GridWidth", UintegerValue (4),
+								   "LayoutType", StringValue ("RowFirst"));
+	mobility.Install (drones);
+
+	mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+								"Mode", StringValue ("Time"),
+								"Time", StringValue ("5s"),
+								"Speed", StringValue ("ns3::UniformRandomVariable[Min=2.0|Max=8.0]"),
+								"Bounds", StringValue ("-1000|1000|-1000|1000"));
+	mobility.SetPositionAllocator("ns3::RandomBoxPositionAllocator",
+								 "X", StringValue ("ns3::UniformRandomVariable[Min=-1000.0|Max=1000.0]"),
+								 "Y", StringValue ("ns3::UniformRandomVariable[Min=-1000.0|Max=1000.0]"),
+								 "Z", StringValue ("ns3::UniformRandomVariable[Min=1.0|Max=1.5]"));
+	mobility.Install (victims);
+}
+
+ApplicationContainer installApps(Ptr<Node> base, NodeContainer trackers)
+{
+	ApplicationContainer servers;
+	ApplicationContainer clients;
+	uint16_t port;
+	Ptr<Node> tracker;
+	Ipv4Address tracker_address;
+	for(uint16_t i = 0; i < nTrackers; ++i)
+	{
+		port = 8000 + i;
+		std::stringstream sdTrace;
+		std::stringstream rdTrace;
+		sdTrace << "evalvid-logs/sd_a01_" << i;
+		rdTrace << "evalvid-logs/rd_a01_" << i;
+
+		tracker = trackers.Get (i);
+		EvalvidServerHelper server(port);
+		server.SetAttribute ("SenderTraceFilename", StringValue("src/evalvid/st_highway_cif.st"));
+		server.SetAttribute ("SenderDumpFilename", StringValue(sdTrace.str()));
+		server.SetAttribute ("PacketPayload", UintegerValue(1024));
+		servers.Add (server.Install (tracker));
+
+		tracker_address = tracker->GetObject<Ipv4>()->GetAddress(1,0).GetLocal();
+		EvalvidClientHelper client (tracker_address,port);
+		client.SetAttribute ("ReceiverDumpFilename", StringValue(rdTrace.str()));
+		clients.Add (client.Install (base));
+	}
+	servers.Start (Seconds (1));
+	servers.Stop (Seconds (simTime-1));
+	clients.Stop (Seconds (simTime-1));
+
+	return clients;
+}
+
+// move node "smoothly" towards the given position
+void move_drone(Ptr<Node> drone, Vector destination, double n_vel) {
+	// get mobility model for drone
+    Ptr<WaypointMobilityModel> mob = drone->GetObject<WaypointMobilityModel>();
+    Vector m_position = mob->GetPosition();
+    double distance = CalculateDistance(destination, m_position);
+	// 1 meter of accuracy is acceptable
+	if(distance <= 1)
+		return;
+
+	unsigned int nodeId = drone->GetId();
+	double currentTime = Simulator::Now().GetSeconds();
+	double nWaypointTime;
+	NS_LOG_UNCOND("moving drone with nodeId: " << nodeId << " from " << m_position << " to " << destination << " time: " << currentTime);
+
+	if(mob->GetVelocity().GetLength() > 0){
+		mob->EndMobility();
+		mob->AddWaypoint(Waypoint(Simulator::Now(), m_position));
+	}
+
+	nWaypointTime = distance/n_vel + currentTime;
+	mob->AddWaypoint(Waypoint(Seconds(nWaypointTime), destination));
+}
+
+int main  (int argc, char *argv[])
+{
+	LogComponentEnable ("EvalvidClient", LOG_LEVEL_INFO);
+	LogComponentEnable ("EvalvidServer", LOG_LEVEL_INFO);
+
+	CommandLine cmd;
+	cmd.AddValue ("netanim", "Enable generation of NetAnim files", enableNetAnim);
+	cmd.Parse (argc, argv);
+
+	NodeContainer firstResponders;
+	firstResponders.Create(1);
+	NodeContainer trackers;
+	trackers.Create(nTrackers);
+	NodeContainer relays;
+	relays.Create(nRelays);
+	NodeContainer victims;
+	victims.Create(nVictims);
+	NodeContainer nodes = NodeContainer (firstResponders, relays, trackers, victims);
+
+	YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+	YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
+	phy.SetChannel (channel.Create ());
+
+	MeshHelper mesh;
+	mesh = MeshHelper::Default ();
+	mesh.SetStackInstaller ("ns3::Dot11sStack", 
+							"Root", Mac48AddressValue (Mac48Address ("00:00:00:00:00:01")));
+
+	//Window when beacon generating starts (uniform random) in seconds
+	mesh.SetMacType ("RandomStart", TimeValue (Seconds (0.1)));
+	//Set number of interfaces per mesh point - default is single-interface mesh point
+	mesh.SetNumberOfInterfaces (1);
+	mesh.SetStandard(WIFI_STANDARD_80211ac);
+	// Install protocols and return container if MeshPointDevices
+	NetDeviceContainer relayDevices;
+	relayDevices = mesh.Install (phy, relays);
+	NetDeviceContainer meshDevices;
+	meshDevices = mesh.Install (phy, NodeContainer (firstResponders, trackers, victims));
+
+	//start relays in off state
+	for(auto iter = relayDevices.Begin(); iter != relayDevices.End(); ++iter)
+	{
+		Ptr<NetDevice> relayInterface = DynamicCast<MeshPointDevice> (*iter)->GetInterface(1);
+		Ptr<WifiNetDevice> relayDevice = DynamicCast<WifiNetDevice> (relayInterface);
+		Ptr<WifiPhy> relayPhy = relayDevice->GetPhy ();
+		relayPhy->SetOffMode();
+	}
+
+	installMobility (firstResponders, NodeContainer(relays, trackers), victims);
+
+	move_drone(trackers.Get (0), victims.Get (0)->GetObject<MobilityModel>()->GetPosition(), 10);
+	move_drone(trackers.Get (1), victims.Get (1)->GetObject<MobilityModel>()->GetPosition(), 10);
+
+	InternetStackHelper internetStack;
+	internetStack.Install (nodes);
+	Ipv4AddressHelper address;
+	address.SetBase ("10.1.1.0", "255.255.255.0");
+	Ipv4InterfaceContainer interfaces;
+	interfaces = address.Assign (meshDevices);
+
+	ApplicationContainer clients;
+	clients = installApps(firstResponders.Get(0), trackers);
+	clients.Start (Seconds (2));
+
+	AnimationInterface* anim;
+	if(enableNetAnim)
+	{
+		anim = new AnimationInterface("ucam-anim.xml");
+		//anim->SetMaxPktsPerTraceFile(2000000); // Set animation interface max packets.
+		for (auto iter = firstResponders.Begin(); iter != firstResponders.End(); ++iter)
+		{
+			Ptr<Node> fr = *iter;
+			anim->UpdateNodeDescription (fr, "First Responder");
+			anim->UpdateNodeColor (fr, 128, 128, 128);
+			anim->UpdateNodeSize (fr->GetId(),5,5);
+		}
+		for (auto iter = trackers.Begin(); iter != trackers.End(); ++iter)
+		{
+			Ptr<Node> tr = *iter;
+			anim->UpdateNodeDescription (tr, "Tracker");
+			anim->UpdateNodeColor (tr, 0, 255, 0);
+			anim->UpdateNodeSize (tr->GetId(),5,5);
+		}
+		for (auto iter = relays.Begin(); iter != relays.End(); ++iter)
+		{
+			Ptr<Node> rl = *iter;
+			anim->UpdateNodeDescription (rl, "Relay");
+			anim->UpdateNodeColor (rl, 0, 0, 255);
+			anim->UpdateNodeSize (rl->GetId(),5,5);
+		}
+		for (auto iter = victims.Begin(); iter != victims.End(); ++iter)
+		{
+			Ptr<Node> vic = *iter;
+			anim->UpdateNodeDescription (vic, "Victim");
+			anim->UpdateNodeColor (vic, 255, 0, 0);
+			anim->UpdateNodeSize (vic->GetId(),5,5);
+		}
+	}
+
+	Simulator::Stop (Seconds (simTime));
+	Simulator::Run ();
+	Simulator::Destroy ();
+	return 0;
+}
