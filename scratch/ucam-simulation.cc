32d80c8164fd4446db161aeaf87d842a90b85fef
Carlos Rocha <carlosh2340@gmail.com>
2020-11-17 14:46:40 -0300

    Add relay positioning algoritm

--- scratch/ucam-simulation.cc
+++ scratch/ucam-simulation.cc
@@ -28,13 +28,21 @@
 #include "ns3/evalvid-client-server-helper.h"
 #include "ns3/netanim-module.h"
 
+#include <list>
+#include <unordered_map>
+#include <cmath>
+
 using namespace ns3;
 
-unsigned int simTime = 100;
-uint16_t nTrackers = 2;
-short nRelays = 10;
+unsigned int simTime = 200;
+short nTrackers = 2;
+short nRelays = 62;
 short nVictims = 2;
 bool enableNetAnim = false;
+list<Ptr<Node>> available_relays;
+unordered_map<unsigned int, list<Ptr<Node>>> relay_chains;
+float max_tx_radius = 45;
+float drone_speed = 10;
 
 void installMobility( NodeContainer firstResponders, NodeContainer drones, NodeContainer victims)
 {
@@ -45,11 +53,11 @@ void installMobility( NodeContainer firstResponders, NodeContainer drones, NodeC
 	mobility.SetMobilityModel("ns3::WaypointMobilityModel",
 								"InitialPositionIsWaypoint", BooleanValue (true));
 	mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-								   "MinX", DoubleValue (-30.0),
-								   "MinY", DoubleValue (-30.0),
+								   "MinX", DoubleValue (-70.0),
+								   "MinY", DoubleValue (-70.0),
 								   "DeltaX", DoubleValue (20.0),
 								   "DeltaY", DoubleValue (20.0),
-								   "GridWidth", UintegerValue (4),
+								   "GridWidth", UintegerValue (8),
 								   "LayoutType", StringValue ("RowFirst"));
 	mobility.Install (drones);
 
@@ -114,13 +122,96 @@ void move_drone(Ptr<Node> drone, Vector destination, double n_vel) {
 	double nWaypointTime;
 	NS_LOG_UNCOND("moving drone with nodeId: " << nodeId << " from " << m_position << " to " << destination << " time: " << currentTime);
 
-	if(mob->GetVelocity().GetLength() > 0){
-		mob->EndMobility();
-		mob->AddWaypoint(Waypoint(Simulator::Now(), m_position));
-	}
+	mob->EndMobility();
+	mob->AddWaypoint(Waypoint(Simulator::Now(), m_position));
 
 	nWaypointTime = distance/n_vel + currentTime;
 	mob->AddWaypoint(Waypoint(Seconds(nWaypointTime), destination));
+}
+
+list<Ptr<Node>>::iterator closest_drone(Vector coordinate, list<Ptr<Node>>& drones){
+	double min;
+	double distance;
+	Vector drone_position;
+	list<Ptr<Node>>::iterator closest;
+
+	auto drone = drones.begin();
+	drone_position = (*drone)->GetObject<MobilityModel>()->GetPosition();
+	min = CalculateDistance(coordinate, drone_position);
+	closest = drone++;
+	
+	for(; drone != drones.end(); ++drone)
+	{
+		drone_position = (*drone)->GetObject<MobilityModel>()->GetPosition();
+		distance = CalculateDistance(coordinate, drone_position);
+		if(distance < min)
+		{
+			min = distance;
+			closest = drone;
+		}
+	}
+	return closest;
+}
+
+void set_wifi_state(Ptr<NetDevice> relay, bool state) {
+	Ptr<NetDevice> relayInterface = DynamicCast<MeshPointDevice> (relay)->GetInterface(1);
+	Ptr<WifiNetDevice> relayDevice = DynamicCast<WifiNetDevice> (relayInterface);
+	Ptr<WifiPhy> relayPhy = relayDevice->GetPhy ();
+	if (state)
+		relayPhy->ResumeFromOff();
+	else
+		relayPhy->SetOffMode();
+}
+
+void update_relay_chains(NodeContainer trackers, Ptr<Node> firstResponders){
+	double distance;
+	double inter_drone_distance;
+	double x,y,z;
+	Vector fr_position = firstResponders->GetObject<MobilityModel>()->GetPosition();;
+	Vector tracker_position;
+	Vector relay_position;
+	Vector relay_destination;
+	Ptr<Node> tracker;
+	list<Ptr<Node>>::iterator allocated_relay;
+	unsigned int relays_required;
+	unsigned int i;
+
+	for(auto iter = trackers.Begin(); iter != trackers.End(); ++iter)
+	{
+		tracker = *iter;
+		tracker_position = tracker->GetObject<MobilityModel>()->GetPosition();;
+		list<Ptr<Node>>& relay_chain = relay_chains[tracker->GetId()];
+
+		distance = CalculateDistance(fr_position, tracker_position);
+		relays_required = (int) ceil(distance / max_tx_radius) - 1;
+		inter_drone_distance = distance / (relays_required + 1);
+		x = inter_drone_distance * (tracker_position.x - fr_position.x) / distance;
+		y = inter_drone_distance * (tracker_position.y - fr_position.y) / distance;
+		z = inter_drone_distance * (tracker_position.z - fr_position.z) / distance;
+		i = 1;
+		for(auto relay : relay_chain)
+		{
+			relay_destination.x = tracker_position.x - i * x;
+			relay_destination.y = tracker_position.y - i * y;
+			relay_destination.z = tracker_position.z - i * z;
+			move_drone(relay, relay_destination, drone_speed+2);
+			++i;
+		}
+
+		while(relay_chain.size() < relays_required && available_relays.size() != 0)
+		{
+			relay_destination.x = tracker_position.x - i * x;
+			relay_destination.y = tracker_position.y - i * y;
+			relay_destination.z = tracker_position.z - i * z;
+			allocated_relay = closest_drone(relay_destination, available_relays);
+			relay_chain.push_back(*allocated_relay);
+			available_relays.erase(allocated_relay);
+			move_drone(*allocated_relay, relay_destination, drone_speed+2);
+			set_wifi_state((*allocated_relay)->GetDevice(0), true);
+			++i;
+		}
+	}
+	Simulator::Schedule(Seconds(1), update_relay_chains, trackers, firstResponders);
 }
 
 int main  (int argc, char *argv[])
@@ -162,19 +253,21 @@ int main  (int argc, char *argv[])
 	NetDeviceContainer meshDevices;
 	meshDevices = mesh.Install (phy, NodeContainer (firstResponders, trackers, victims));
 
+	available_relays.assign (relays.Begin(), relays.End());
+
 	//start relays in off state
 	for(auto iter = relayDevices.Begin(); iter != relayDevices.End(); ++iter)
 	{
-		Ptr<NetDevice> relayInterface = DynamicCast<MeshPointDevice> (*iter)->GetInterface(1);
-		Ptr<WifiNetDevice> relayDevice = DynamicCast<WifiNetDevice> (relayInterface);
-		Ptr<WifiPhy> relayPhy = relayDevice->GetPhy ();
-		relayPhy->SetOffMode();
+		set_wifi_state (*iter, false);
 	}
 
 	installMobility (firstResponders, NodeContainer(relays, trackers), victims);
 
-	move_drone(trackers.Get (0), victims.Get (0)->GetObject<MobilityModel>()->GetPosition(), 10);
-	move_drone(trackers.Get (1), victims.Get (1)->GetObject<MobilityModel>()->GetPosition(), 10);
+	for(auto iter = trackers.Begin(); iter != trackers.End(); ++iter)
+	{
+		Ptr<Node> tracker = *iter;
+		relay_chains[tracker->GetId()] = list<Ptr<Node>>();
+	}
 
 	InternetStackHelper internetStack;
 	internetStack.Install (nodes);
@@ -185,7 +278,11 @@ int main  (int argc, char *argv[])
 
 	ApplicationContainer clients;
 	clients = installApps(firstResponders.Get(0), trackers);
-	clients.Start (Seconds (2));
+	clients.Start (Seconds (110));
+
+	move_drone(trackers.Get (0), victims.Get (0)->GetObject<MobilityModel>()->GetPosition(), drone_speed);
+	move_drone(trackers.Get (1), victims.Get (1)->GetObject<MobilityModel>()->GetPosition(), drone_speed);
+	update_relay_chains(trackers, firstResponders.Get(0));
 
 	AnimationInterface* anim;
 	if(enableNetAnim)
